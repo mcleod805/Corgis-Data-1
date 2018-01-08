4ee1fbbb0f7c2c99b15434a320170fcd14496a79
McLeodN <31251637+mcleod805@users.noreply.github.com>
2018-01-07 17:52:18 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 def render_response():
     state = request.args["Car Companies"]
     return render_template('page1.html',options = get_state_options(), response = your_interesting_demographic_function(state))
-def get_state_options():
-    options = "from flask import Flask, request, Markup, render_template, Markup"
 
 @app.route("/home")
 def render_response():
@@ -25,6 +23,10 @@ def get_state_options():
     for s in cars:
        options += Markup("<option value=\"" + s + "\">" + s + "</option>")
     return options
+def your_interesting_demographic_function(stateName):
+    with open('cars (1).json') as cars_data: 
+    return stateName
+        
 @app.route("/")
 def render_main():
     return render_template('index.html')
